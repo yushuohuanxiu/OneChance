5cec4e4e9b5ff8171396a06b5d7d713d03a997df
yushuohuanxiu <moonshine_1988@yeah.net>
2016-09-12 00:58:43 +0800

    Update OneChance.sol

--- contracts/OneChance.sol
+++ contracts/OneChance.sol
@@ -88,14 +88,19 @@ contract OneChance {
     // 代币合约
     OneChanceCoin public oneChanceCoin;
     
+    struct User {
+        address userAddr; // 用户账户
+        bytes32 ciphertext; // 用户购买 Chance 时提交的 sha3(随机数)
+        uint plaintext; // Chance 售罄后通知用户提交的 原始随机数
+    }
+    
     struct Goods {
         string name; // 奖品名称
         uint amt; // 奖品价格
         string description; // 奖品描述
-        address[] consumerArr; // 购买了 Chance 的用户列表
-        bytes32[] ciphertextArr; // 用户购买 Chance 时提交的 sha256(随机数)
-        uint[] plaintextArr; // Chance 售罄后通知用户提交的 随机数
-        uint winner; // 中奖用户，等于 sum(所有用户随机数)%amt
+        uint alreadySale;
+        mapping (uint => User) consumerMap; // 购买了 Chance 的用户列表
+        uint winner; // 中奖用户，等于 sum(所有用户随机数)%amt+1
     }
     
     // 商品列表
@@ -124,19 +129,23 @@ contract OneChance {
     }
    
     // 发布奖品,只有主办方可以调用
-    function postGoods(string _name, uint _amt, string _description) onlySponsor returns (bool) {
-        topGoodsIndex += 1;
-        goodsMap[topGoodsIndex] = Goods({name:_name, amt:_amt, description:_description, consumerArr: new address[](_amt), ciphertextArr: new bytes32[](_amt), plaintextArr: new uint[](_amt), winner:0});
+    function postGoods(string _name, uint _amt, string _description) onlySponsor {
+        Goods memory goods;
+        goods.name = _name;
+        goods.amt = _amt;
+        goods.description = _description;
+        topGoodsIndex++;
+        goodsMap[topGoodsIndex] = goods;
         // 通知主办方发布成功
         PostGoods(msg.sender, _name, _amt, _description, topGoodsIndex);
-        return true;
     }
    
     // 购买 Chance
-    function buyChance(uint _goodsId, bytes32[] _ciphertextArr) returns (bool) {
-        if (goodsMap[_goodsId].consumerArr.length + _ciphertextArr.length > goodsMap[_goodsId].amt) throw;
+    function buyChance(uint _goodsId, bytes32[] _ciphertextArr) {
+        Goods goods = goodsMap[_goodsId];
+        if (goods.alreadySale + _ciphertextArr.length > goods.amt) throw;
         for (uint i=0; i<_ciphertextArr.length; i++) {
-            if (sha256(0) == _ciphertextArr[1]) throw;
+            if (sha3(0) == _ciphertextArr[i]) throw;
         }
        
         // 扣减用户 ChanceCoin
@@ -144,67 +153,52 @@ contract OneChance {
         
         // 记录商品的购买用户
         for (i=0; i<_ciphertextArr.length; i++) {
-            goodsMap[_goodsId].consumerArr.push(msg.sender);
-            goodsMap[_goodsId].ciphertextArr.push(_ciphertextArr[i]);
+            goods.alreadySale++;
+            goods.consumerMap[goods.alreadySale] = User(msg.sender, _ciphertextArr[i], 0);
         }
         
         // 通知用户购买成功
         BuyChance(msg.sender, _goodsId, _ciphertextArr.length);
+        
         // 如果商品 Chance 已售罄，通知购买用户提交原始随机数以生成中奖用户
-        if (goodsMap[_goodsId].consumerArr.length == goodsMap[_goodsId].amt) {
+        if (goods.alreadySale == goods.amt) {
             notify(_goodsId);
         }
-        return true;
     }
     
     // 通知用户提交原始随机数
     function notify(uint _goodsId) private {
-        for (uint i=0; i<goodsMap[_goodsId].consumerArr.length; i++) {
-            NotifySubmitPlaintext(goodsMap[_goodsId].consumerArr[i], _goodsId, i, goodsMap[_goodsId].ciphertextArr[i]);
+        Goods goods = goodsMap[_goodsId];
+        for (uint i=1; i<=goods.amt; i++) {
+            NotifySubmitPlaintext(goods.consumerMap[i].userAddr, _goodsId, i, goods.consumerMap[i].ciphertext);
         }
     }
     
-    // 提交原始随机数,随机数应在 1-amt 中取值，避免超过 uint 表示范围
+    // 提交原始随机数,随机数应在 1-9223372036854775807 中取值(web3.js的toHex函数最大可以处理的值)
     function submitPlaintext(uint _goodsId, uint _userId, uint _plaintext) {
-        if (_plaintext > goodsMap[_goodsId].amt) throw;
-        if (goodsMap[_goodsId].plaintextArr[_userId] != 0) {
+        Goods goods = goodsMap[_goodsId];
+        if (_plaintext > 9223372036854775807) throw;
+        if (goods.consumerMap[_userId].plaintext != 0) { // 如果原始随机数已提交过,不重复做写操作
             SubmitPlaintext(msg.sender, _goodsId, _userId);
             throw;
         }
-        if (sha256(_plaintext) != goodsMap[_goodsId].ciphertextArr[_userId]) throw;
+        if (sha3(_plaintext) != goods.consumerMap[_userId].ciphertext) throw;
         
-        goodsMap[_goodsId].plaintextArr[_userId] = _plaintext;
+        goods.consumerMap[_userId].plaintext = _plaintext;
         SubmitPlaintext(msg.sender, _goodsId, _userId);
         
-        uint winner;
-        for (uint i=0; i<goodsMap[_goodsId].amt; i++) {
-            if (goodsMap[_goodsId].plaintextArr[i] == 0) {
+        uint plaintextSum;
+        for (uint i=1; i<=goods.amt; i++) {
+            if (goods.consumerMap[i].plaintext == 0) {
                 return;
             } else {
-                winner += goodsMap[_goodsId].plaintextArr[i];
+                plaintextSum += goods.consumerMap[i].plaintext;
             }
         }
         
         // 计算中奖用户
-        goodsMap[_goodsId].winner = winner % goodsMap[_goodsId].amt;
+        goods.winner = plaintextSum % goodsMap[_goodsId].amt;
         
     }
-    
-    function getGoods(uint _index) returns (string name, uint amt, string description, uint winner, address[] consumerArr, bytes32[] ciphertextArr, uint[] plaintextArr) {
-        name = goodsMap[_index].name;
-        amt = goodsMap[_index].amt;
-        description = goodsMap[_index].description;
-        winner = goodsMap[_index].winner;
-        consumerArr = goodsMap[_index].consumerArr;
-        ciphertextArr = goodsMap[_index].ciphertextArr;
-        plaintextArr = goodsMap[_index].plaintextArr;
-    }
-    
-    function getGoodsInfo(uint _index) returns (string name, uint amt, string description, uint alreadySale, uint winner) {
-        name = goodsMap[_index].name;
-        amt = goodsMap[_index].amt;
-        description = goodsMap[_index].description;
-        alreadySale = goodsMap[_index].amt - goodsMap[_index].consumerArr.length;
-        winner = goodsMap[_index].winner;
-    }
+
 }
